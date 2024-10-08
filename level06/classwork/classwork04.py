8fe3d00c47525af37f9c5c163d6e8d5e88562eed
notanakapanadze <ananokapanadze.2209@gmail.com>
2024-10-08 21:27:47 +0400

    Add files via upload

---
+++ level06/classwork/classwork04.py
@@ -0,0 +1,13 @@
+name=input("enter your name")
+surname=input("enter your surname")
+print(name,surname)
+
+name1=input("enter a nummber")
+name2=input("enter a another nummber")
+print(int(name1)+int(name2))
+
+print(int(name1)-int(name2))
+
+print(int(name1)*int(name2))
+
+print(int(name1)//int(name2))
