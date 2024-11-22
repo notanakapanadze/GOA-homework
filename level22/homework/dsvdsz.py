b6ec1cfe81290b74633e0e2f2a320bbf6af4719f
notanakapanadze <ananokapanadze.2209@gmail.com>
2024-11-22 20:59:36 +0400

    Add files via upload

---
+++ level22/homework/dsvdsz.py
@@ -0,0 +1,29 @@
+# დავალება 1
+list_of_names = ["დავით", "ნინო", "გიორგი", "დავით", "მარიამი", "დავით"]
+name_to_count = "დავით"
+count = list_of_names.count(name_to_count)
+print(count)
+
+# დავალება 2
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse()
+print(numbers)
+
+# დავალება 3
+list_of_numbers = [1, 2, 3]
+length = len(list_of_numbers)
+result = list_of_numbers * length
+print(result)
+
+#და ვალება 4
+insert_arr = ["ნინო", "გიორგი", "მარიამი"]
+insert_arr.insert(1, "დავითი")
+print(insert_arr)
+
+# დავალება 5
+sample_list = ["დავით", "ნინო", "დავით", "მარიამი", "დავით"]
+element_to_remove = "დავით"
+count = sample_list.count(element_to_remove)
+sample_list.remove(element_to_remove)
+print(count)
+print(sample_list)
