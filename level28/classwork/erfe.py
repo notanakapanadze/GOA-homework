d434ef1a8d559cf442d43ea44f9cd8cf7240d53f
notanakapanadze <ananokapanadze.2209@gmail.com>
2024-12-12 19:25:22 +0400

    Add files via upload

---
+++ level28/classwork/erfe.py
@@ -0,0 +1,12 @@
+def square(n):
+    return n * n
+print(square(5))  
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+print(even_or_odd(4))  
+print(even_or_odd(7))  
